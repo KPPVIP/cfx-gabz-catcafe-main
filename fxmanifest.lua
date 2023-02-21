cd143e60c3d26935acec8c1548467a40a323681e
Guudz <Kilian1702@outlook.com>
2023-02-21 15:28:51 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'npguys:)'
+description 'Gabz Cat Cafe'
+version '0.1.0'
+
+this_is_a_map 'yes'
+
+server_scripts {
+    "@mysql-async/lib/MySQL.lua",
+    "sync/genpi1"
+  }
